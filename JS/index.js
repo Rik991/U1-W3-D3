0e406c563aac0c84e52e2c21060a5a7576a8fc0d
Riccardo Santilli <riccardosantilli@live.it>
2024-08-28 17:51:01 +0200

    work fine

--- JS/index.js
+++ JS/index.js
@@ -4,24 +4,24 @@ const toDoList = document.querySelector("#TaskList");
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  function handleCompleteTask() {
-    newTask.classList.toggle("complete");
-  }
-  function handleDeleteTask() {
-    newTask.remove();
-    deleteButton.remove();
-  }
-
   const tsInput = document.getElementById("tsInput");
   const tsOutput = tsInput.value;
   const newTask = document.createElement("li");
   const deleteButton = document.createElement("button");
-  deleteButton.innerText = "elimina";
+  deleteButton.innerText = "elimina"; //deleteButton.innerText = "🗑️"; con " win + . " troviamo le emoji
   newTask.innerText = tsOutput;
+  newTask.appendChild(deleteButton);
   toDoList.appendChild(newTask);
-  toDoList.appendChild(deleteButton);
   form.reset();
 
   newTask.addEventListener("click", handleCompleteTask);
   deleteButton.addEventListener("click", handleDeleteTask);
 };
+
+function handleCompleteTask(event) {
+  event.currentTarget.classList.toggle("complete");
+}
+function handleDeleteTask(event) {
+  //qui anche se questa funzione verrà chiamata dal deletButton con il parentNode riesco ad eliminare tutto l'elemento li (task e bottone)
+  event.currentTarget.parentNode.remove();
+}
